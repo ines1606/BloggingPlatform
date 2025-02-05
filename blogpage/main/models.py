4da314412e0f3e14a814520e9bd75c8798fd3a93
Ines <hamestuk.ines@web.de>
2025-02-05 23:58:14 +0100

    added DB support for BlogPost

--- blogpage/main/models.py
+++ blogpage/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=1000)
+    content = models.CharField(max_length=8000)
+    publish_date = models.DateTimeField(auto_now_add=True)
+
+class Tags(models.Model):
+    text = models.CharField(max_length=1000)
+    blogPost = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
