29cf60b9e1657df36f842b779dad7dc05fbd9732
Ines <hamestuk.ines@web.de>
2025-02-05 16:20:49 +0100

    added Sign Up page

---
+++ blogpage/accounts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login") #redirect to login upon successful registration
+    template_name = "registration/signup.html"
+
