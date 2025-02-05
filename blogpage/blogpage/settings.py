8880f9f38119725eab04f763305fcad7e6f10b6b
Ines <hamestuk.ines@web.de>
2025-02-05 23:15:50 +0100

    enabled sign up, added articles html page

--- blogpage/blogpage/settings.py
+++ blogpage/blogpage/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'accounts'
+    'accounts',
     'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
