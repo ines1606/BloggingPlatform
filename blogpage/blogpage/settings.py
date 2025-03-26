c44606f5e2cc8628741e6ba0d3d81bd40d8a61a9
Ines <hamestuk.ines@web.de>
2025-03-26 18:16:25 +0100

    improved UX

--- blogpage/blogpage/settings.py
+++ blogpage/blogpage/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -136,7 +136,7 @@ CONTACT_REDIRECT_URL = "contact"
 # TODO: (maybe) set up SMTP server
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "main/static"
